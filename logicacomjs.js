1e10993ead3e59e3ffacd50e9889b5828f1f9c5c
TheCaioLobo <caiomatheuslobodias@gmail.com>
2025-05-22 17:08:30 -0300

    aprendendo prompt

---
+++ logicacomjs.js
@@ -0,0 +1,37 @@
+/*let Bruna=6.5
+Carla=8
+let soma=Bruna+Carla
+console.log("A soma é "+soma)
+console.log("--------------------")
+
+let dividaemjaneiro = 400;
+let pagoemfevereiro = 300;
+let oquantodevo = dividaemjaneiro-pagoemfevereiro
+console.log("A subtração da divida pelo valor pego, restou a pagar; "+oquantodevo+"R$")
+console.log("--------------------")
+
+let totalParcelas = 10;
+let totalDivida = 500;
+let valorParcela = totalDivida / totalParcelas;
+
+console.log("A divisão da dívida pelo número de parcelas resultou em: " + valorParcela + " R$ por parcela");
+console.log("--------------------");
+
+let Multiplicacao = 75;
+let quantidadeParcelas = 6;
+let Divida = Multiplicacao * quantidadeParcelas;
+
+console.log("A multiplicação pela quantidade de parcelas resultou em: " + Divida + " R$");
+
+let nome_do_usuario = "Ryan";
+let salario = 2500;
+let meses_trad = 8
+let decimo = salario/12*meses_trad
+console.log(nome_do_usuario+"seu decimo é: "+ decimo+ " parabems")*/
+
+
+let salario = Number (prompt ("Digite seu salario"))
+let mestrab = Number(prompt ("Digite seus meses trabalhados"))
+let decimo = Number(prompt ("Digite seus meses trabalhados"))
+console.log(x)
+
